32ce81346af8012e0578c9674ca8e521f960c093
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-03-27 15:03:12 +0530

    first commit

---
+++ src/main/java/com/auto/care/autoserviceapisrc/AutoServiceApiSrcApplication.java
@@ -0,0 +1,13 @@
+package com.auto.care.autoserviceapisrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoServiceApiSrcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoServiceApiSrcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auto/care/autoserviceapisrc/AutoServiceApiSrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auto.care.autoserviceapisrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoServiceApiSrcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
